1a429bc9201209d730a775990a61c20bbd1ee825
Alando <alandoappling@gmail.com>
2021-11-06 12:23:38 -0400

    Update request.py

--- pytrends/request.py
+++ pytrends/request.py
@@ -141,6 +141,8 @@ class TrendReq(object):
         # but occasionally it sends 'application/javascript
         # and sometimes even 'text/javascript
         try:
+            print(response)
+            print(response.headers)
             if response.status_code == 200 and 'application/json' in \
                     response.headers['Content-Type'] or \
                     'application/javascript' in response.headers['Content-Type'] or \
